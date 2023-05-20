7b70454af1e02cd9d61498d10c61e61a6194b5df
ali.sleem <ali.sleem@fixedmea.com>
2023-05-20 14:34:11 +0300

    deploy

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs')
-const path = require('path')
+const path = require('path');
+const Todo = require('../models/todo');
+const verify = require('../middlewares/verify');
 // todos
 // create todos
-router.post('/',(req,res)=>{
-	res.send('create user')
-
+router.post('/',verify,async(req,res)=>{
+	const createdTodo = await Todo.create({
+		title:req.body.title,
+		status:req.body.status,
+		user:req.user._id
+	})
+	res.send(createdTodo);
 })
 // gettodos
-router.get('/',(req,res,next)=>{
+router.get('/',verify,async(req,res,next)=>{
+	const todos = await Todo.find({user:req.user._id}).populate('user');
+	res.json(todos)
 	// const filter = {}
 	// const {status,title} = req.query;
 	// if(status) filter.status = status;
